d254cf00b2d63340785e6d8697371b9df2c242b3
Himanshu <himanshuramchandani08@gmail.com>
2022-01-30 12:36:03 +0530

    day 8 javascript while loop typeof questions

---
+++ 3_JavaScript/8_JavaScript While loop datatypes/script.js
@@ -0,0 +1,46 @@
+// while 
+
+var i = 1;
+var s = "";
+while (i <= 10){
+    s = s + i + " <br>"
+    i += 1;
+}
+
+document.getElementById('one').innerHTML = s;
+
+// wap enter a number from user and 
+//print reverse of it.
+
+// wap enter a number from user and print 
+//sum of itys individual digits.
+
+// wap to enter a number from 
+//user and print factorial of it.
+
+// 1 - 10
+// 3 , 5 -- 3 + 5 + 6 + 9 = 23
+
+// 1 - 1000
+//15 X
+
+n = prompt("Enter a number");
+
+console.log(typeof "10");
+
+console.log(typeof 10);
+console.log(typeof 10.5);
+console.log(typeof "A");
+console.log(typeof "India");
+console.log(typeof 0);
+console.log(typeof 1);
+console.log(typeof true);
+
+// 2520
+// 1 - 10
+
+// ?
+// 1 - 20
+
+n = Math.floor(n/10);
+console.log(n);
