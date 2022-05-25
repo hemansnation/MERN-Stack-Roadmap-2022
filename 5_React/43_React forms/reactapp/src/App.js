9a909540473d5d1ffe0a057cef9e064ada43814a
Himanshu <himanshuramchandani08@gmail.com>
2022-05-25 19:02:13 +0530

    Day 43 React forms

---
+++ 5_React/43_React forms/reactapp/src/App.js
@@ -0,0 +1,102 @@
+import { Component } from 'react';
+
+class App extends Component{
+    
+    state = {
+        firstName: '',
+        lastName: '',
+        country: '',
+        title: '',
+    }
+
+    handleChange = (e) => {
+        // const value = e.target.value
+
+        // this.setState({
+        //     firstName: value
+        // })
+        const { name, value } = e.target
+
+        this.setState({
+            [name]: value
+        })
+
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+
+        console.log(this.state);
+    }
+    
+    render() {
+        const { firstName, lastName, country, title } = this.state
+
+        return (
+            <div>
+                {/* <label htmlFor='firstName' >First Name: </label>
+                <input 
+                    type='text'
+                    id='firstName'
+                    name='firstName'
+                    placeholder='First Name'
+                    value={firstName}
+                    onChange={this.handleChange}
+                />
+
+                <h1>{this.state.firstName}</h1> */}
+
+                <div>
+                    <h3>This is a Form</h3>
+
+                    <form onSubmit={this.handleSubmit}>
+                        <div>
+                            <input 
+                                type='text'
+                                name='firstName'
+                                placeholder='First Name'
+                                value={firstName}
+                                onChange={this.handleChange}
+                            />
+                        </div>
+                        <div>
+                            <input 
+                                type='text'
+                                name='lastName'
+                                placeholder='Last Name'
+                                value={lastName}
+                                onChange={this.handleChange}
+                            />
+                        </div>
+                        <div>
+                            <input 
+                                type='text'
+                                name='country'
+                                placeholder='Country'
+                                value={country}
+                                onChange={this.handleChange}
+                            />
+                        </div>
+                        <div>
+                            <input 
+                                type='text'
+                                name='title'
+                                placeholder='Title'
+                                value={title}
+                                onChange={this.handleChange}
+                            />
+                        </div>
+
+                        <button className='btn btn-success'>
+                            Submit
+                        </button>
+                        
+                    </form>
+                </div>
+
+            </div>
+        )
+    }
+}
+
+export default App
